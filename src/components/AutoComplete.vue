9c79d3296edabdb789b572ee0640b73dc1328dc2
JufeBrown <jufebrown@gmail.com>
2022-06-08 15:47:42 -0500

    add autoComplete component

---
+++ src/components/AutoComplete.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <label for="name-input">Name:</label>
+    <input id="name-input" type="text" />
+    <div>
+      <ul></ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "AutoComplete",
+  computed: {
+    ...mapGetters(["nameList"]),
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
